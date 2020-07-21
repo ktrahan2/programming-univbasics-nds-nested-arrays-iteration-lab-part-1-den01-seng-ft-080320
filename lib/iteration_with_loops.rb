2d478d12ad73fd1ec152eac6c4316e5c3f37f6fb
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-21 01:03:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,9 +5,9 @@ def find_even_values(src)
     inner_count = 0
     while inner_count < src[count].length do
       src[count][inner_count]
-      inner_count += 0
+      inner_count += 1
     end
-    count += 0
+    count += 1
   end
 end
 
